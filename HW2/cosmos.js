6f8e6ad51435c8f3e8bbf3fce5ecd702344692eb
BlueBug <benson.liu.himself@gmail.com>
2020-03-22 10:42:33 +0800

    click to stop marquee

--- HW2/cosmos.js
+++ HW2/cosmos.js
@@ -4,3 +4,12 @@ $(function() {
       $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
   });
 });
+
+
+$(document).ready(function(){
+  $('.pg2_text').on('mousedown touchstart',function(){
+    $(this).css('animation-play-state','paused')
+  }).bind('mouseup mouseleave touched',function(){
+    $(this).css('animation-play-state','running')
+  })
+})

f9a9342b36a2034189c670106fd85c67820df398
BlueBug <benson.liu.himself@gmail.com>
2020-03-25 21:47:58 +0800

    arrow animation

--- HW2/cosmos.js
+++ HW2/cosmos.js
@@ -19,9 +19,61 @@ $(function(){
     $('#section04').css('filter', 'grayscale(50%)')
   })
 })*/
-
+/*
 $(function(){
   $('.fade').hover(function(){
     $(this).transition('fly down',300)
   })
+})*/
+/*
+$(function(){
+ $('.arrow').mouseenter(function(){
+    $('.arrow').addClass('home')
+    $(this).removeClass('arrow')
+ })
 })
+
+$(function(){
+  $('.home').mouseleave(function(){
+    console.log('hello')
+    $('.home').addClass('arrow')
+    $(this).removeClass('home')
+  })
+})*/
+$(function(){
+  $('.home').on('mouseenter',function(){
+    $('.arrow').removeClass('hidden transition')
+    $(this).transition('fade up',200)
+    $('#see').css('visibility','hidden')
+    $(this).removeClass('home hidden transition')
+    $('.arrow').css('visibility','visible')
+    $('.arrow').addClass('animated fadeInUp')
+    
+  })
+})
+$(function(){
+  $('.arrow').on('click',function(){
+    $('#see').removeClass('hidden transition')
+    $(this).transition('fade up',200)
+    $(this).removeClass('hidden transition')
+    $(this).css('visibility','hidden')
+    $('#see').css('visibility','visible')
+    $('#see').addClass('home')
+  })
+})
+/*
+$(function(){
+  $('.arrow').click(function(){
+    console.log("test")
+    //$(this).removeClass('animated fadeInUp')
+   // $(this).transition('fade up',200)
+    //$(this).css('visibility','none')
+    $('.home').removeClass('hidden transition')
+    $(this).transition('fade up',200)
+   // $('.home').css('visibility','visible')
+   /// $('.home').addClass('animated fadeInUp')
+    $(this).removeClass('hidden transition')
+   // $('.home').removeClass('hidden transition')
+   // $(this).css('visibility','none')
+  })
+})*/
